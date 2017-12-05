e48d0e9150917ef32b4c5fd15934085887eefa99
Mikael <mikael.ek@cygni.se>
2017-12-05 14:25:17 +0100

    wip

--- flow.js
+++ flow.js
@@ -1,6 +1,6 @@
 module.exports = {
-  "extends": ["plugin:flowtype/recommended"],
-  "plugins": [
-    "flowtype",
+  extends: ['plugin:flowtype/recommended'],
+  plugins: [
+    'flowtype',
   ],
 }
