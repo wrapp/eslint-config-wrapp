dfbe58c6060e606532558ba4ccbe94e37ce130e5
emattias <mattiasha@gmail.com>
2018-02-08 17:49:25 +0100

    enable require-valid-file-annotation

--- flow.js
+++ flow.js
@@ -1,5 +1,8 @@
 module.exports = {
   extends: ['plugin:flowtype/recommended'],
+  rules: {
+    'flowtype/require-valid-file-annotation': [ 2, 'always' ],
+  },
   plugins: [
     'flowtype',
   ],
