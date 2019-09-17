f4331518fafb47d79b936bc7783ab4157eb2ee3e
Baldur Árnason <baldurarna@meniga.com>
2019-09-17 14:09:35 +0000

    add react hooks plugin

--- react.js
+++ react.js
@@ -1,12 +1,17 @@
 module.exports = {
-  extends: [
-    './base.js',
-    'airbnb/rules/react',
-    'airbnb/rules/react-a11y',
-    'eslint-config-prettier/react',
-  ],
-  rules: {
-    'react/jsx-filename-extension': 0,
-    'react/require-default-props': 0,
-  },
+	plugins: [
+		'react-hooks',
+	],
+	extends: [
+		'./base.js',
+		'airbnb/rules/react',
+		'airbnb/rules/react-a11y',
+		'eslint-config-prettier/react',
+	],
+	rules: {
+		'react/jsx-filename-extension': 0,
+		'react/require-default-props': 0,
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
+	},
 }
